02c158cdc2e332c3dc70135a876a74df97a1ce78
asp <asp@Aptechnk1.edu>
2024-11-11 10:43:02 +0500

    Crud Application

---
+++ CRUD_Application/Models/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Application.Models
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options): base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CRUD_Application/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_Application.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage="Please Enter your name")]
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter your Email")]
+        [Column(TypeName = "varchar(50)")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please Enter your Phone Number")]
+        [Column(TypeName = "varchar(50)")]
+        public string Phone { get; set; }
+    }
+}
